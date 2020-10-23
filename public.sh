e623253218d422010a1d2347ab78d1601a155834
yuunk <dchipself0@gmail.com>
2020-10-23 22:37:35 +0900

    torima:

--- public.sh
+++ public.sh
@@ -1,3 +1,3 @@
 cp -r static/build/* instagram-clone-api/public/
 
-# mv instagram-clone-api/public/index.html instagram-clone-api/resources/views/welcome.blade.php
+mv instagram-clone-api/public/index.html instagram-clone-api/resources/views/welcome.blade.php
